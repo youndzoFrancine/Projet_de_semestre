59dfc9f1a12c3199e7a32451163c5351f9e29816
alexandre.gabrielli <33123187+AlexandreGabrielli@users.noreply.github.com>
2019-03-24 13:19:56 +0100

    Update useranddb.sql

--- script_db/useranddb.sql
+++ script_db/useranddb.sql
@@ -22,11 +22,48 @@ CREATE DATABASE "Forum"
     CONNECTION LIMIT = -1
     TEMPLATE template0;
 	
+	CREATE TABLE Departement(
+	departementID	SERIAL PRIMARY KEY,
+	nomDepartement	VARCHAR NOT NULL 
+);
+
+CREATE TABLE Roles(
+	roleID		SERIAL PRIMARY KEY,
+	nomRole		VARCHAR NOT NULL 
+);
+
+CREATE TABLE Utilisateur (
+	utilisateurID	SERIAL PRIMARY KEY,
+	nomUtilisateur	VARCHAR NOT NULL,
+	mailUtilisateur	VARCHAR NOT NULL,
+	motDePasse  	VARCHAR NOT NULL,
+	roleUtilisateur	INTEGER NOT NULL,
+    FOREIGN KEY (roleUtilisateur) REFERENCES Roles(roleID) 
+);
+
+CREATE TABLE Discussion (
+	discussionID	SERIAL PRIMARY KEY,
+	msgRacineID		INTEGER, 
+	utilisateurID	INTEGER NOT NULL,
+    FOREIGN KEY (utilisateurID) REFERENCES Utilisateur(utilisateurID)
+);
+
+CREATE TABLE Message (
+	messageID		SERIAL PRIMARY KEY,
+	message			VARCHAR, 
+	score			INTEGER,
+	utilisateurID	INTEGER NOT NULL,
+	discussionID	INTEGER NOT NULL,
+	superMessageID  INTEGER, 
+    FOREIGN KEY (utilisateurID) REFERENCES Utilisateur(utilisateurID),
+	FOREIGN KEY (discussionID) REFERENCES discussion(discussionID) 
+);
+
+CREATE TABLE Tag (
+	valeur			VARCHAR,
+	prioritaire		BOOLEAN, 
+	rang			INTEGER 
+);
 	
-
-
- 
-
-
-
-
+ALTER TABLE Discussion 
+	ADD CONSTRAINT constraint_name FOREIGN KEY (msgRacineID) REFERENCES Message(messageID);
