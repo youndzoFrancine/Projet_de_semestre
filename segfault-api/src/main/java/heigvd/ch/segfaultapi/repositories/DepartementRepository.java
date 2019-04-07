bcbe63a36c41342dbb5676413cc802354817ea8f
Crescence Yimnaing <37827277+CrescenceK@users.noreply.github.com>
2019-04-07 15:19:22 +0200

    Revert " script d insertion dans la DB"

--- segfault-api/src/main/java/heigvd/ch/segfaultapi/repositories/DepartementRepository.java
+++
@@ -1,8 +0,0 @@
-package heigvd.ch.segfaultapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import heigvd.ch.segfaultapi.model.Departement;
-
-public interface DepartementRepository extends JpaRepository<Departement, Integer> {
-
-}
