34d16b00dc1882cadfe431ccb3662d3e40015365
crescy <crescy@yahoo.com>
2019-04-07 14:34:27 +0200

    Insertion utilisateur

--- script_db/insertions.sql
+++ script_db/insertions.sql
@@ -38,3 +38,18 @@ Values
 	('#wireshark', false, 2);
 COMMIT;
 
+ALTER SEQUENCE utilisateur_utilisateur_id_seq RESTART WITH 1;
+INSERT into utilisateur(nom_utilisateur, mail_utilisateur, mot_de_passe, role_utilisateur)
+Values
+  ('Admin', 'admin.administrateur@heig-vd.ch', 'admin#123', 4),
+  ('CrescenceK', 'crescence.yimnaingkamdem@heig-vd.ch', 'cyk123', 1),
+  ('youndzoFrancine', 'francine.youndzokengne@heig-vd.ch', 'fyk456', 1),
+  ('tiagoquin', 'tiago.povoaqui@heig-vd.ch', 'tpq789', 1),
+  ('cedriclankeu', '(cedric.lankeungassam@heig-vd.ch', 'cln123', 1),
+  ('AlexandreGabrielli', 'alexandre.gabrielli@heig-vd.ch', 'agi456', 1),
+  ('Maxcaduff', 'max.caduff@heig-vd.ch', 'mcf789', 1),
+  ('grafolytics', 'marcel.graf@heig-vd.ch', 'mgf123', 3),
+  ('ci4cb', 'carlos.pena@heig-vd.ch', 'cpa456', 3),
+  ('nionel', 'lionel.assistant@heig-vd.ch', 'lionel', 2),
+  ('nicole', 'nicole.assistant@heig-vd.ch', 'nicole', 2);
+COMMIT;
