761b85cfecfb746e5c64fe2854c199b3c7754f6a
crescy <crescy@yahoo.com>
2019-04-07 14:05:34 +0200

    Insertion tag

--- script_db/insertions.sql
+++ script_db/insertions.sql
@@ -17,4 +17,24 @@ Values
 	('Administrateur'),  
 COMMIT;
 
+ALTER SEQUENCE tag_tag_id_seq RESTART WITH 1;
+INSERT into tag(nom, prioritaire, rang)
+Values
+	('#debian', false, 1),
+	('#poo', true, 5),
+	('#pro', true, 8),
+	('#assembleur', false, 2),
+	('#numerisation', true, 4),
+	('#signauxetsysteme', true, 5),
+	('#gen', true, 7),
+	('#scapy', false, 2),
+	('#cpp', true, 3),
+	('#netbeans', true, 5),
+	('#postgresql', true, 5),
+	('#nodejs', true, 6),
+	('#securite', true, 9),
+	('#get', false, 2),
+	('#angular', true, 7),
+	('#wireshark', false, 2);
+COMMIT;
 
