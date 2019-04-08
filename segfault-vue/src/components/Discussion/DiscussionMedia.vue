65d23c5e85547387b22b7bb8b160050492de3312
Tiago Póvoa <tiago.quin@gmail.com>
2019-04-08 19:04:07 +0200

    DiscuMediaItem

--- segfault-vue/src/components/Discussion/DiscussionMedia.vue
+++ segfault-vue/src/components/Discussion/DiscussionMedia.vue
@@ -1,50 +1,18 @@
 <template>
-  <div class="discussion-media">
-    <article v-bind:key="post.id" v-for="post in posts" class="media">
-      <figure class="media-left">
-        <p class="image is-64x64">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </p>
-      </figure>
-      <div class="media-content">
-        <div class="content">
-          <p>{{post.title}}</p>
-          <p>
-            <strong>posté par @shellmaster</strong>
-          </p>
-          <p>
-            <small>dernière réponse 14 avril</small>
-          </p>
-        </div>
-        <nav class="level is-mobile">
-          <div class="level-left">
-            <a class="level-item">
-              <span class="icon is-small">
-                <i class="fas fa-reply"></i>
-              </span>
-            </a>
-            <a class="level-item">
-              <span class="icon is-small">
-                <i class="fas fa-retweet"></i>
-              </span>
-            </a>
-            <a class="level-item">
-              <span class="icon is-small">
-                <i class="fas fa-heart"></i>
-              </span>
-            </a>
-          </div>
-        </nav>
-      </div>
-      <div class="media-right">
-        <button class="delete"></button>
-      </div>
-    </article>
+  <div class="discussions">
+    <div v-bind:key="post.id" v-for="post in posts" class="discussion-media">
+      <DiscuMediaItem v-bind:post="post"/>
+    </div>
   </div>
 </template>
 
 <script>
-export default { name: "DiscussionMedia", props: ["posts"], components: {} };
+import DiscuMediaItem from "@/components/Discussion/DiscuMediaItem.vue";
+export default {
+  name: "DiscussionMedia",
+  props: ["posts"],
+  components: { DiscuMediaItem }
+};
 </script>
 
 <style lang="scss" scoped>
