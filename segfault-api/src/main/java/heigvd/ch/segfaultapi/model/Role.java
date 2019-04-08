4144cfa707c5c57a16a62642f0de8807223610ca
youndzoFrancine <francine.youndzokengne@heig-vd.ch>
2019-04-08 10:38:19 +0200

    class Role fixed

--- segfault-api/src/main/java/heigvd/ch/segfaultapi/model/Role.java
+++ segfault-api/src/main/java/heigvd/ch/segfaultapi/model/Role.java
@@ -1,4 +1,38 @@
 package heigvd.ch.segfaultapi.model;
 
+import heigvd.ch.segfaultapi.model.Utilisateur;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+import java.util.Set;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "Roles")
 public class Role {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleID;
+
+    @Column(name = "nomRole")
+    @NonNull
+    private String nomRole;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<Utilisateur> utilisateurs;
+
+    public Role(String nomRole){
+        this.nomRole=nomRole;
+    }
+
+    public void setNomRole(String nomRole) {
+        this.nomRole = nomRole;
+    }
+
+    public void setUtilisateurs(Set<Utilisateur> utilisateurs) {
+        this.utilisateurs = utilisateurs;
+    }
 }
