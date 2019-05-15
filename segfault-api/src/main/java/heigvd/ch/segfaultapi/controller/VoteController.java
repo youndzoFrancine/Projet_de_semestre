7ba31ca6f534e14961354f72b7bfbcc94dde7d97
Tiago Póvoa <tiago.quin@gmail.com>
2019-05-15 15:54:40 +0200

    find vote by user id

--- segfault-api/src/main/java/heigvd/ch/segfaultapi/controller/VoteController.java
+++ segfault-api/src/main/java/heigvd/ch/segfaultapi/controller/VoteController.java
@@ -1,15 +1,14 @@
 package heigvd.ch.segfaultapi.controller;
 
 import heigvd.ch.segfaultapi.model.Vote;
-import heigvd.ch.segfaultapi.repositories.RoleRepoitory;
+import heigvd.ch.segfaultapi.model.Utilisateur;
+import heigvd.ch.segfaultapi.repositories.UtilisateurRepository;
 import heigvd.ch.segfaultapi.repositories.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8080")
@@ -19,8 +18,20 @@ public class VoteController {
     @Autowired
     private VoteRepository voteRepository;
 
+    @Autowired
+    UtilisateurRepository utilisateurRepository;
+
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<Vote> getAll() {
         return voteRepository.findAll();
     }
+
+    @RequestMapping(value ="/{id}", method = RequestMethod.GET)
+    public List<Vote> getById (@PathVariable("id") Integer id) {
+
+        Utilisateur utilisateur = utilisateurRepository.findById(id).get();
+
+        return voteRepository.findAllByUtilisateur(utilisateurRepository.findById(id).get());
+    }
+
 }
--- segfault-api/src/main/java/heigvd/ch/segfaultapi/repositories/VoteRepository.java
+++ segfault-api/src/main/java/heigvd/ch/segfaultapi/repositories/VoteRepository.java
@@ -1,9 +1,13 @@
 package heigvd.ch.segfaultapi.repositories;
 
+import heigvd.ch.segfaultapi.model.Utilisateur;
 import heigvd.ch.segfaultapi.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VoteRepository extends JpaRepository<Vote, Integer> {
+import java.util.List;
 
+
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+    List<Vote> findAllByUtilisateur(Utilisateur utilisateur);
 }
 
